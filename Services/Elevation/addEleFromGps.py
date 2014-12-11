0373e0f92b0b70758d00c918fab1009a05f1c393
timestring <timestringalpha@gmail.com>
2014-12-11 11:42:30 -0800

    addEleFromGps update

--- Services/Elevation/addEleFromGps.py
+++ Services/Elevation/addEleFromGps.py
@@ -1,7 +1,7 @@
 # imports
-from ElevationService import ElevationRequester
+import ElevationService
 
-filename = 'baro_n501_20141208_211251'    # sample: baro_n501_20141208_211251, without TWO levels of extension plz...
+filename = 'baro_n501_20141207_181427'    # sample: baro_n501_20141208_211251, without TWO levels of extension plz...
 
 rootDir = '../../Data/forMat/'
 
@@ -9,8 +9,6 @@ src = rootDir + filename + '.gps.csv'
 dst = rootDir + filename + '.gpsele.csv'
 
 # create elevation requester
-rq = ElevationRequester()
-rq.setSaveName('ttmp')
 
 f = open(src)
 lines = [ x.strip().split(',') for x in f.readlines() ]
@@ -18,17 +16,11 @@ f.close()
 
 lines = [ x for x in lines if x[-1] == '0' ]
 queryList = [ ( float(x[1]), float(x[2]) ) for x in lines ]
-
-rq.setPositions(queryList)
-rq.requestElevations()
-
-f = open('storage/ttmp.csv')
-res = f.readlines()
-f.close()
+res = ElevationService.requestElevations(queryList)
 
 assert len(lines) == len(res)
 
 f = open(dst, 'w')
 for (q, r) in zip(lines, res):
-	f.write( ",".join( tuple( q[0:3] + [ r.strip().split(',')[-1] ] ) ) + '\n')
+	f.write( ",".join( tuple( q[0:3] + [ str(r) ] ) ) + '\n')
 f.close()

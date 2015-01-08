308e4320a5b6df7a01d760e90c4b40d69ff328e6
Paul Martin <pdmart@gmail.com>
2015-01-08 15:10:46 -0800

    increased max requested points

--- Services/Elevation/getUclaElevation.py
+++ Services/Elevation/getUclaElevation.py
@@ -4,9 +4,9 @@ from ElevationService import *
 # get UCLA GPS coords
 latlng_NW = (34.078567, -118.460890)
 latlng_SE = (34.059868, -118.432823)
-resolution = 1e-4
+resolution = 1e-5
 requester = ElevationGridRequester(latlng_NW,latlng_SE,resolution)
 requester.downloadElevations()
-requester.saveAsFolder('storage/Ucla')
+requester.saveAsFolder('storage/Ucla_1em5')
 
 

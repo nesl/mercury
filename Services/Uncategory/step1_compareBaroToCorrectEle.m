5453723b945afc89c53c421de0756c75d62cd8bb
timestring <timestringalpha@gmail.com>
2015-01-12 18:56:21 -0800

    rename and more analysis on 1st step - compare raw data to ground truth elevation

--- Services/Uncategory/step1_compareBaroToCorrectEle.m
+++ Services/Uncategory/step1_compareBaroToCorrectEle.m
@@ -51,9 +51,29 @@ for seaPre = 1018:0.5:1022
         x = [1 seaPre sca W(end, end)]
         answer = [answer; x];
         [p, q, W] = dtw_baro(flipud(datae)', height');
-        x = [1 seaPre sca W(end, end)]
+        x = [2 seaPre sca W(end, end)]
         answer = [answer; x];
     end
 end
 
 res = sortrows(answer, 4)
+
+
+% lesson learned here:
+%    if the offset (seaPre) is correct, the scale coefficient (sca)
+%    doesn't matter that much
+
+%% parse res
+f = res(res(:,1) == 1,:);
+f(:,4) = log10(f(:,4));
+m = zeros(9, 11);
+for i = 1:size(f,1)
+    a = round((f(i,2) - 1018) / 0.5) + 1;
+    b = round((f(i,3) + 8.08) / 0.02) + 1;
+    m(a, b) = f(i,4);
+end
+clf
+imagesc(m)
+set(gca, 'XTickLabel', strread(num2str(-8.08:0.02:-7.88), '%s') )
+set(gca, 'YTickLabel', strread(num2str(1018:0.5:1022), '%s') )
+colorbar

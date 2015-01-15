029dc0b09800d4a1ffc237d37aaa9cf320ebed0f
Bo-Jhang <TimeStringAlpha@gmail.com>
2015-01-15 10:14:58 -0800

    push back the matrix

--- Services/Elevation/getUclaElevation.py
+++ Services/Elevation/getUclaElevation.py
@@ -6,7 +6,7 @@ latlng_NW = (34.049785, -118.471726)
 latlng_SE = (34.088381, -118.419076)
 resolution = 1e-4
 requester = ElevationGridRequester(latlng_NW,latlng_SE,resolution)
-requester.downloadElevations('storage/Ucla_large')
+requester.downloadElevations('storage/Ucla_large', False)
 #requester.saveAsFolder('storage/Ucla_large')
 
 

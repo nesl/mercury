4cd915b8ecad8fe7296d7e3f98a75454e5843a60
timestring <timestringalpha@gmail.com>
2015-01-18 17:58:50 -0800

    create dtw figures for poster

--- Services/SearchPath/searchForATruePath.m
+++ Services/SearchPath/searchForATruePath.m
@@ -1,5 +1,11 @@
-eleTrajDir = '../../Data/eleSegments/ucla_west/';
-baroFile = '../../Data/eleSegments/test_case/case1_baro_gnd.csv';
+% input files
+eleTrajDir = '../../Data/eleSegments/ucla_small/';
+baroFile = '../../Data/eleSegments/test_case/case2_baro_query.csv';
+
+% output files
+MAX_RESULTS = 20;
+outPath = '../../Data/resultSets/';
+outFileName = 'case2_ucla_small_hilgard_results.rset';
 
 tic
 
@@ -40,7 +46,7 @@ for i = 1:numel(endNodePairs)
     nextNodes{nna} = [nextNodes{nna} nnb];
     nextNodes{nnb} = [nextNodes{nnb} nna];
 end
-fprintf('finish reading all the trajectories\n')
+fprintf('finish reading all the trajectories, %d nodes, %d segments\n', nrNode, numel(endNodePairs))
 
 % segment barometer trajectory into window
 dataq = csvread(baroFile);
@@ -57,9 +63,10 @@ for i = 1:size(dataq,1)
 end
 baros = baros ./ baroc;
 baros = baros(setdiff(1:nrB, find(isnan(baros))));
+nrB = length(baros);   % since some invalid windows are taken out thus total # of windows changes
 
-seaPre = 1020;
-sca = -8.15;
+seaPre = 1019.394;
+sca = -7.9736;
 height = (baros - seaPre) * sca;
 
 % all pair DTW
@@ -120,11 +127,7 @@ sortedTraces = nestedSortStruct(traces, {'score'});
 fprintf('computation time %.2f\n', toc);
 
 %% Generate result output
-MAX_RESULTS = 5;
-fpath = '../../Data/resultSets/';
-fname = 'ucla_west_results.rset';
-
-fid = fopen([fpath fname], 'w');
+fid = fopen([outPath outFileName], 'w');
 for i=1:min( MAX_RESULTS, length(sortedTraces) )
     score = sortedTraces(i).score;
     t = sortedTraces(i).trace(:,1);
@@ -138,4 +141,49 @@ for i=1:min( MAX_RESULTS, length(sortedTraces) )
     fprintf(fid,'\n');
 end
 
+return
 
+%% debug
+detrace = sortedTraces(1).trace;
+deele = [];
+for i = 1:length(detrace)-1
+    a = nodeName2ind(num2str(detrace(i)));
+    b = nodeName2ind(num2str(detrace(i+1)));
+    deele = [deele; eleTrajs{a, b}];
+end
+
+clf
+hold on
+plot(deele, 'r')
+plot(height)
+
+case2trajList = [
+122762254
+122568569
+566556466
+122762256
+122762258
+122579087
+1956573659
+122762229
+1717275967
+122762231
+122762234
+122762238
+122762242
+122762246
+122681077
+122681080
+122681083
+];
+deeleG = [];
+for i = 1:length(case2trajList)-1
+    a = nodeName2ind(num2str(case2trajList(i)));
+    b = nodeName2ind(num2str(case2trajList(i+1)));
+    deeleG = [deeleG; eleTrajs{a, b}];
+end
+plot(deeleG, 'g')
+
+%% debug session 2
+x = all_pair_dtw_baro(deeleG', height');
+x(1,end)

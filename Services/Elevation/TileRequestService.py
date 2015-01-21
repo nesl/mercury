96e98f3f140a8529fb6a1203e0410e2854dd66b8
Prashanth <prashanthsw@gmail.com>
2015-01-20 17:10:34 -0800

    Added requester service that accepts a center location with width of surrounding area in miles, as well as a defined box; the service simply determines the appropriate tile starting locations and requests only one point per tile from ElevationService. Should reduce time ElevationService checks for pre-existing tiles. Most information is in ElevationService already, so would suggest adding functionality directly there in the future.

---
+++ Services/Elevation/TileRequestService.py
@@ -0,0 +1,36 @@
+from ElevationService import *
+import math
+
+class TileRequester:
+	
+	def __init__(self, resolution=4):
+		elev_service = ElevationRequester(resolution)
+		self.tileResolution = elev_service.tileResolution
+	
+	def _getBoundedBox(center_lat, center_lon, miles):
+		df = miles/69.0
+		dl = df/math.cos(math.radians(center_lat))
+		return [center_lat + df, center_lon - dl, center_lat - df, center_lon + dl]                
+	
+	# Provide bounds to get tiles on as list: [NWlong, NWlat, SElong, SElat]
+	def _getMinimalTileSet(self, bounds):
+		tiledBounds = [self.tileResolution*int(x/self.tileResolution) for x in bounds]
+		longVertCnt = abs(int((tiledBounds[0] - tiledBounds[2])/self.tileResolution))
+		latVertCnt = abs(int((tiledBounds[1] - tiledBounds[3])/self.tileResolution))
+
+		tileCorners = []
+		for x in range(0, longVertCnt):
+			longCol = tiledBounds[0] + x*self.tileResolution
+			for y in range(0, latVertCnt):
+				latRow = tiledBounds[1] + y*self.tileResolution
+				tileCorners.append((longCol,latRow));
+
+		return tileCorners
+	
+	def requestTiles(self, bounds):
+		if(len(bounds) == 3):
+			bounds = self._getBoundedBox(bounds[0], bounds[1], bounds[2])
+		elif(len(bounds) != 4):
+			raise Exception('Bad arguments')
+		tileSet = self._getMinimalTileSet(bounds)
+		elev_service.request(tileSet)

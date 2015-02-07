d47e06a85a8e411fe1bc6877c888212218b5bec3
timestring <timestringalpha@gmail.com>
2015-02-06 19:59:55 -0800

    deleted accidentally duplicated files

--- Analysis/PathFinding/utils/all_pair_dtw_baro.m
+++ Analysis/PathFinding/utils/all_pair_dtw_baro.m
@@ -24,10 +24,10 @@ c = numel(hbaro);
 
 re = nan(c);
 
-%costMatrix = (repmat(hele, 1, c) - repmat(hbaro, r, 1)) .^ 2;
-costMatrix = exp(repmat(hele, 1, c) - repmat(hbaro, r, 1));
+costMatrix = (repmat(hele, 1, c) - repmat(hbaro, r, 1)) .^ 2;   % square
+%costMatrix = ( abs(repmat(hele, 1, c) - repmat(hbaro, r, 1)) ) .^ 3 + 0.2;   % cube
+%costMatrix = exp( abs(repmat(hele, 1, c) - repmat(hbaro, r, 1)) );  % exponential
 %costMatrix
-size(costMatrix)
 
 % costs
 for i = 1:c
@@ -43,14 +43,11 @@ for i = 1:c
     % store the DTW result back to D
     for j = 1:r; 
         for k = 1:cc;
-            D(j+1,k+1) = D(j+1,k+1) + min([D(j, k), D(j, k+1), D(j+1, k)]);
+            %D(j+1,k+1) = D(j+1,k+1) + min([D(j, k), D(j, k+1), D(j+1, k)]);
+            D(j+1,k+1) = D(j+1,k+1) + min([D(j, k), D(j+1, k)]);
         end
     end
     
     % copy the last row back to return matrix
     re(i, i:end) = D(end, 2:end);
 end
-
-re = costMatrix;
-size(costMatrix)
-size(re)

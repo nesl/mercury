c5ddaa80bd1d274d93114fca485d1c93bf1ffaea
pdmart <pdmart@gmail.com>
2015-02-07 00:14:21 -0800

    updated greedy solver with EXP = 1, rewarding longer paths

--- Analysis/PathFinding/testPartialDTW.m
+++ Analysis/PathFinding/testPartialDTW.m
@@ -7,21 +7,36 @@ s1 = sin(t).^2 + 2*cos(0.5*t + 0.2);
 s2 = -cos(t) - sin(0.2*t) - t + 0.1*t.^2 - 3;
 
 %% Partial DTW
-p1 = s1(1:40);
-p1 = p1 + 0.05*randn(size(p1)) - 0.5;
-p1 = imresize(p1, [1 80]);
-p2 = 5+ s2(1:40);
-p2 = p2 + 0.05*randn(size(p2));
+L_arr = [];
+c2_arr = [];
+c1_arr = [];
 
-plot(s1);
-hold on;
-plot(p1,'r','LineWidth',3);
-plot(p2,'m--','LineWidth',2);
-hold off;
-figure()
 
-c2 = DTW_greedy(s1,p2);
-c1 = DTW_greedy(s1,p1);
-plot(c1(end,:),'r');
+for L = 10:5:200
+    p1 = s1(1:L);
+    p1 = p1 + 0.05*randn(size(p1)) - 0.5;
+    p1 = imresize(p1, [1 round(L*1.5)]);
+    p2 = 5+ s2(1:40);
+    p2 = p2 + 0.05*randn(size(p2));
+    
+    % plot(s1);
+    % hold on;
+    % plot(p1,'r','LineWidth',3);
+    % plot(p2,'m--','LineWidth',2);
+    % hold off;
+    % figure()
+    
+    c2 = DTW_greedy(s1,p2);
+    c1 = DTW_greedy(s1,p1);
+    % plot(c1(end,:),'r');
+    % hold on;
+    % plot(c2(end,:),'m');
+    
+    c2_arr = [c2_arr; c2];
+    c1_arr = [c1_arr; c1];
+    L_arr = [L_arr; L];
+end
+
+plot(L_arr, c1_arr, 'r');
 hold on;
-plot(c2(end,:),'m');
+plot(L_arr, c2_arr, 'm--');
--- Analysis/PathFinding/utils/DTW_greedy.m
+++ Analysis/PathFinding/utils/DTW_greedy.m
@@ -1,4 +1,4 @@
-function [ costs ] = DTW_greedy( template, partial )
+function [ greedy_cost ] = DTW_greedy( template, partial )
 %DTW_GREEDY Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -24,7 +24,23 @@ costMatrix = (repmat(partial, 1, num_cols) - repmat(template', num_rows, 1)) .^ 
 
 %% Find shortest path using dynamic programming
 [~, ~, Costs] = dp(costMatrix);
-costs = Costs;
+
+% the greedy (partial) cost is the minimum cost solution that matches up to
+% SOME point of the template, not necessarily the end of the template.
+% However, if we don't reward longer matches somehow, we'll end up with
+% trivially short solutions and our solver won't want to explore new paths.
+
+% Therefore, the GREEDY cost should be the combination of cost and length
+% that minimizes an expression like the following:
+
+% cost/( length^e )
+
+% where the exponent 'e' is a design variable.  Something between 0 and 1?
+EXP = 1;
+match_costs = Costs(end,:);
+lengths = 1:size(Costs,2);
+length_weights = 1./( lengths.^EXP );
+greedy_cost = min( match_costs.*length_weights );
 
 end
 

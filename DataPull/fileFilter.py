f9d9a29fe1d39820d439362cfb5c2c8e9ce39a2a
Bo-Jhang <TimeStringAlpha@gmail.com>
2015-02-26 01:17:10 -0800

    new event added

--- DataPull/fileFilter.py
+++ DataPull/fileFilter.py
@@ -1,14 +1,14 @@
 import os
 
 def do():
-    accExt = ['acc', 'baro', 'gps', 'gyro', 'mag']
+    accExt = ['acc', 'baro', 'gps', 'gyro', 'mag', 'offset', 'event']  # accepted extension
 
     cmd = 'adb shell ls storage/sdcard0/ > tmp'
     os.system(cmd)
 
     f = open('tmp')
     names = [ x.strip() for x in  f.readlines() if x[:5] == 'baro_']
-    names = [ x for x in names if 29 <= len(x) and len(x) <= 30 and x.split('.')[-1] in accExt]
+    names = [ x for x in names if len(x.split('.')[0]) == 25 and x.split('.')[-1] in accExt]
     f.close()
 
     os.system('rm tmp')

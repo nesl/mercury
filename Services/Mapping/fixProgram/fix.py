702b7ae35527356323455cf7c5c6e473ae90c7c4
Bo-Jhang <TimeStringAlpha@gmail.com>
2015-02-27 21:30:20 -0800

    again fix bugs of fix.py

--- Services/Mapping/fixProgram/fix.py
+++ Services/Mapping/fixProgram/fix.py
@@ -31,15 +31,21 @@ for i in range(1, len(lines)):
 nextNodes = {}   # node-> list of neighbor nodex
 for e in edges:
     (na, nb) = e
+    if na == nb:  # i don't know why this can happen in osm
+        continue
+
     if na not in nextNodes:
         nextNodes[na] = []
-    nextNodes[na] += [nb]
+    if nb not in nextNodes[na]:  # it's strange neighbor nodes can appear several times
+        nextNodes[na] += [nb]
+
     if nb not in nextNodes:
         nextNodes[nb] = []
-    nextNodes[nb] += [na]
+    if na not in nextNodes[nb]:  # it's strange neighbor nodes can appear several times
+        nextNodes[nb] += [na]
 
-print('Total # of nodes: ' + str(len(nodes)))
-print('Total # of edges: ' + str(len(edges)))
+sys.stderr.write('Total # of nodes: ' + str(len(nodes)) + '\n')
+sys.stderr.write('Total # of edges: ' + str(len(edges)) + '\n')
 #asking = ['122905200', '1903812544', '1903812541', '1903812539', '1903812527', '1903812524', '122905197']
 #for ask in asking:
 #    print ask, nextNodes[ask]
@@ -67,21 +73,36 @@ for n in nextNodes:
 
         if len(criticalNodes) < 100:
             continue
-        print('find component with size ' + str(len(criticalNodes)))
+        sys.stderr.write('find component with size ' + str(len(criticalNodes)) + '\n')
 
+        idx = 0
         for nn in criticalNodes:
+            idx += 1
+            #sys.stderr.write(str(idx) + '\n')
             for tn in nextNodes[nn]:
                 if (nn, tn) not in tracedEdges:
                     seg = [nn, tn]
+                    #sys.stderr.write(str(nn) + ' ->' + str(tn))
                     tracedEdges.add( (nn, tn) )
                     tracedEdges.add( (tn, nn) )
                     while seg[-1] not in criticalNodes:
                         nnn = seg[-1]
+                        find = 0
+                        #sys.stderr.write(' ')
                         for tnn in nextNodes[nnn]:
                             if (nnn, tnn) not in tracedEdges:
                                 tracedEdges.add( (nnn, tnn) )
-				tracedEdges.add( (tnn, nnn) )
+                                tracedEdges.add( (tnn, nnn) )
                                 seg += [tnn]
+                                #sys.stderr.write('->' + str(tnn))
+                                find += 1
+                        if find != 1:
+                            sys.stderr.write('error\n')
+                            print >>sys.stderr, nnn
+                            print >>sys.stderr, nextNodes[nnn]
+                            print >>sys.stderr, (nnn in criticalNodes)
+                            quit()
                     segCoor = [ x + ',' + nodes[x][0] + ',' + nodes[x][1] for x in seg ]
+                    #sys.stderr.write('\n')
                     print ",".join( tuple(segCoor) )
 

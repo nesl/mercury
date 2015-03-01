0b5e226705a9582f55f146c3f65af1df4e5f9290
Prashanth <prashanthsw@gmail.com>
2015-03-01 00:16:51 -0800

    Entropy calculation function

--- Analysis/PathFinding/calculateMapEntropy.m
+++ Analysis/PathFinding/calculateMapEntropy.m
@@ -1,9 +1,9 @@
 function mapEntropy = calculateMapEntropy(mapData, binWidth, sequenceLen, numSliding)
-    [latlngNE, latlngSW] = map_data.getBoundaryCoordinates();
-    maxLat = latlngNE(1);
-    maxLng = latlngNE(2);
-    minLat = latlngSW(1);
-    minLng = latlngSW(2);
+    %[latlngNE, latlngSW] = mapData.getBoundaryCoordinates();
+    %maxLat = latlngNE(1);
+    %maxLng = latlngNE(2);
+    %minLat = latlngSW(1);
+    %minLng = latlngSW(2);
     
     % let assumes later I'll narrow down into a smaller area, specified as
     % the above area
@@ -11,6 +11,19 @@ function mapEntropy = calculateMapEntropy(mapData, binWidth, sequenceLen, numSli
     % calculate entropy...
     % for each segment
     %   ...
+    
+    segments = mapData.getAllSegments();                                    % Grab all segments
+    signatures = [];
+    for seg = 1:size(segments,1)                                            
+        segElev = ceil(mapData.getSegElev(segments(seg,:))/binWidth);       % Bin all elevations
+        for win=1:numSliding:size(segElev,1)-sequenceLen+1                  % For each window
+            signatures = [signatures; segElev(win:win+sequenceLen-1)'];     % Store window bin
+        end
+    end
+    [~,~,set] = unique(signatures, 'rows');                                 % Get indices
+    tb = tabulate(set);                                                     % Get frequencies
+    freqs = tb(:,3)/100;
+    mapEntropy = sum(-freqs*log(freqs));                                    % Calculate entropy
 end
 
 % test:

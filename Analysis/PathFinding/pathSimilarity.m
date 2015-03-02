5586dfc7e15d113773590a86b870928d16e4f1ce
timestring <timestringalpha@gmail.com>
2015-03-02 11:51:40 -0800

    ho

--- Analysis/PathFinding/pathSimilarity.m
+++ Analysis/PathFinding/pathSimilarity.m
@@ -1,20 +1,26 @@
 clc; clear; clf;
 add_paths
 
+map_manager = MapManager();
+for mapID = [41 2 23 7]
+for mapSize = [4]
+
 %mapfile =    '../../Data/EleSegmentSets/ucla_5x5/';
 %map_data = MapData(mapfile);
-map_manager = MapManager();
-map_data = map_manager.getMapDataObject(41, 3, 1);  % Seattle
+
+%map_data = map_manager.getMapDataObject(41, 3, 1);  % Seattle
 %map_data = map_manager.getMapDataObject(38, 3, 1);  % San Francisco
 %map_data = map_manager.getMapDataObject(2, 3, 1);  % Atlanta
 %map_data = map_manager.getMapDataObject(23, 3, 1);  % Los Angeles
 %map_data = map_manager.getMapDataObject(29, 3, 1);  % New York
 %map_data = map_manager.getMapDataObject(7, 3, 1);  % Chicago
 
+map_data = map_manager.getMapDataObject(mapID, 4, 1);  % Chicago
+
 %% Random Walk Parameters
 path_length = 100; % number of elevation points
 flag_allow_go_back = 0; % if 'allow_go_back', you can return to a node by an intermediary.
-expected_visited_time = 1;
+expected_visited_time = 2;
 
 totalDistance = map_data.getTotalDistanceOfAllSegments();
 num_paths = floor(totalDistance / 10 / path_length * expected_visited_time);
@@ -37,7 +43,15 @@ parfor i = 1:num_paths
 end
 fprintf('finish calculating dtw scores\n');
 
+matName = ['simi-map' num2str(mapID) '-size' num2str(mapSize)];
+save(matName, 'scores');
+matName = ['simi-map' num2str(mapID) '-size' num2str(mapSize) '-paths'];
+save(matName, 'elevs');
 
+end
+end
+
+%%
 imagesc(log10(scores+1));
 colorbar
 return;
